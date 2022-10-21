f34923d2a0cee375e72fbfc8263717e17b2a7289
cristinxmurillo <murilloccristina@gmail.com>
2022-10-21 14:28:46 +0100

    refactored into class
    refactored grid into a class and separated actions into method so it can be a more independent file. Expanded the screen to add buttons and stuff.

--- grid.py
+++ grid.py
@@ -1,6 +1,6 @@
 import pygame
 
-def drawGrid(gridSize):
+class Grid:
 
     # color palette
     black = (0, 0, 0)
@@ -8,30 +8,43 @@ def drawGrid(gridSize):
     blue = (159, 172, 230)
     red = (255, 0, 0)
 
-    # display configs
-    size = 600
-    padding = 20
-    horizontalStepSize = (size - padding * 2)/gridSize[0]
-    verticalStepSize = (size - padding * 2)/gridSize[1]
-    screen = pygame.display.set_mode([size, size], pygame.RESIZABLE)
-    pygame.display.set_caption("Pathfinding algorithm visualizer")
-    screen.fill(white)
+    def __init__(self, screen, gridSize):
+        self.gridSize = gridSize
+        self.screen = screen
+        self.size = 600
+        self.padding = 20
+        pygame.display.set_caption("Pathfinding algorithm visualizer")
+     
+    def getStepSizes(self):
+        #Grid will always be (600px, 600px) with padding of 20px
+        horizontalStepSize = (self.size - self.padding * 2)/self.gridSize[0]
+        verticalStepSize = (self.size - self.padding * 2)/self.gridSize[1]
+        
+        return [horizontalStepSize, verticalStepSize]
 
-    pygame.draw.rect(screen, blue, [padding, padding, (size - padding * 2),  (size - padding * 2)])
+    def drawGrid(self):
+        
+        horizontalStepSize, verticalStepSize = self.getStepSizes()
+        
+        self.screen.fill(grid.white)
 
-    # grid drawing
-    # vertical lines 
-    for i in range(0, gridSize[0] + 1):
-        pygame.draw.line(screen, black, (padding + horizontalStepSize * i, padding), (padding + horizontalStepSize * i, size - padding), 1)
-    for i in range(0, gridSize[0] + 1):
-        pygame.draw.line(screen, black, (padding, padding + verticalStepSize * i), (size - padding, padding + verticalStepSize * i), 1)
+        pygame.draw.rect(self.screen, grid.blue, [self.padding, self.padding, (self.size - self.padding * 2),  (self.size - self.padding * 2)])
 
-    pygame.display.update()
-    
-    while(True):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
+        # grid drawing
+        # vertical lines 
+        for i in range(0, self.gridSize[0] + 1):
+            pygame.draw.line(self.screen, grid.black, (self.padding + horizontalStepSize * i, self.padding), (self.padding + horizontalStepSize * i, self.size - self.padding), 1)
+        for i in range(0, self.gridSize[0] + 1):
+            pygame.draw.line(self.screen, grid.black, (self.padding, self.padding + verticalStepSize * i), (self.size - self.padding, self.padding + verticalStepSize * i), 1)
 
+        pygame.display.update()
+        
 if __name__ == "__main__":
-    drawGrid([100, 100])
+    screen = pygame.display.set_mode([800, 600])
+    grid = Grid(screen, [5, 5])
+    grid.drawGrid()
+
+    while(True):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
